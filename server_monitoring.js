da30cca7f49b94db25ca7442aa8aaded40dc3b4e
Weslley Oliveira Araujo <woa@certi.org.br>
2022-02-01 02:04:57 -0300

    Refactor code to receive parameters from arguments

--- server_monitoring.js
+++ server_monitoring.js
@@ -1,24 +1,20 @@
 var http = require('http');
 var log4js = require("log4js");
+const args = require('minimist')(process.argv.slice(2))
 
 var count = 0;
 log4js.configure({
-    appenders: { cheese: { type: "file", filename: "cheese.log" } },
+    appenders: { cheese: { type: "file", filename: `${args['ip']}${args['url']}.log` } },
     categories: { default: { appenders: ["cheese"], level: "error" } }
   });
 var logger = log4js.getLogger("cheese");
 logger.level = "debug";
 
 var options = {
-    host: '192.168.15.2',
-    path: '/measurements'
+    host: args['ip'],
+    path: args['url'],
   };
-
-  var options2 = {
-    host: '192.168.15.2',
-    path: '/logs_pt'
-  };
-  
+ 
   callback = function(response) {
     var str = '';
   
@@ -45,17 +41,34 @@ var options = {
     });
   }
   
-  function myFunc(arg) {
+  function getUrlContent(arg) {
       try {
+        if (!args['url']) {
+            console.log("parameter --url is required");
+        
+        }
+        if (!args['ip']) {
+          console.log("parameter --ip is required");
+        
+      }
+      if (!args['interval']) {
+        console.log("parameter --interval is required");
+      
+    }
+
+      if (!args['ip'] || !args['url'] || !args['interval']) {
+       return;
+      }
+
+        console.log(`trying to reach ${args['ip']+ args['url']}`);
+        console.log(args['url']);
         http.request(options, callback).end();
-        http.request(options2, callback).end();
         count++;
         logger.debug(`requests count ${count}'`);
-        console.log(`arg was => ${arg}`);
       } catch (error) {
         logger.debug(error);
       }
    
   }
   
-  setInterval(myFunc, 1500, 'ServerMonitoring');
+  setInterval(getUrlContent, args['interval'], 'ServerMonitoring');
